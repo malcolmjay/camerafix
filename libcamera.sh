508225787d09276977fe554356f7e7380234feaf
malcolmjay <35127786+malcolmjay@users.noreply.github.com>
2026-03-16 23:43:22 -0500

    Update libcamera.sh

--- libcamera.sh
+++ libcamera.sh
@@ -20,36 +20,46 @@ cleanup_on_error() {
 trap cleanup_on_error ERR
 
 if [[ "${EUID}" -eq 0 ]]; then
-  fail "Please run this script as the pi user, not as root. Use: bash reinstall_will_libcamera.sh"
+  fail "Please run this script as the pi user, not as root. Use: bash libcamera.sh"
 fi
 
 cd "$HOME"
 
+log "Checking kernel version"
+uname -r
+
 log "Updating package lists"
 sudo apt update
 
-log "Installing libcamera build dependencies"
+log "Installing required build dependencies"
 sudo apt install -y \
   git \
   meson \
   ninja-build \
+  cmake \
+  pkg-config \
   python3-pip \
   python3-jinja2 \
   python3-ply \
   python3-yaml \
+  python3-dev \
   libyaml-dev \
   libboost-program-options-dev \
   libdrm-dev \
+  libudev-dev \
   libexif-dev \
   libepoxy-dev \
   libjpeg-dev \
-  libtiff5-dev \
   libpng-dev \
+  libtiff-dev \
+  libtiff5-dev \
+  libgnutls28-dev \
+  openssl \
   libavcodec-dev \
   libavdevice-dev \
   libavformat-dev \
   libswresample-dev \
-  cmake
+  pybind11-dev
 
 log "Removing existing libcamera source directory"
 rm -rf "$HOME/libcamera"
@@ -57,8 +67,13 @@ rm -rf "$HOME/libcamera"
 log "Cloning Will Whang's libcamera repository"
 git clone https://github.com/will127534/libcamera.git "$HOME/libcamera"
 
-log "Configuring libcamera"
+log "Entering libcamera directory"
 cd "$HOME/libcamera"
+
+log "Removing any old libcamera build directory"
+rm -rf build
+
+log "Configuring libcamera"
 meson setup build \
   --buildtype=release \
   -Dpipelines=rpi/vc4,rpi/pisp \
@@ -79,22 +94,8 @@ ninja -C build
 log "Installing libcamera"
 sudo ninja -C build install
 
-log "Installing rpicam-apps build dependencies"
-sudo apt install -y \
-  cmake \
-  libboost-program-options-dev \
-  libdrm-dev \
-  libexif-dev \
-  libepoxy-dev \
-  libjpeg-dev \
-  libtiff5-dev \
-  libpng-dev \
-  meson \
-  ninja-build \
-  libavcodec-dev \
-  libavdevice-dev \
-  libavformat-dev \
-  libswresample-dev
+log "Refreshing linker cache after libcamera install"
+sudo ldconfig
 
 log "Removing existing rpicam-apps source directory"
 rm -rf "$HOME/rpicam-apps"
@@ -103,13 +104,18 @@ log "Cloning Will Whang's rpicam-apps repository"
 cd "$HOME"
 git clone https://github.com/will127534/rpicam-apps.git "$HOME/rpicam-apps"
 
-log "Configuring rpicam-apps"
+log "Entering rpicam-apps directory"
 cd "$HOME/rpicam-apps"
+
+log "Removing any old rpicam-apps build directory"
+rm -rf build
+
+log "Configuring rpicam-apps"
 meson setup build \
-  -Denable_libav=enabled \
+  -Denable_libav=disabled \
   -Denable_drm=enabled \
   -Denable_egl=enabled \
-  -Denable_qt=enabled \
+  -Denable_qt=disabled \
   -Denable_opencv=disabled \
   -Denable_tflite=disabled
 
@@ -122,6 +128,9 @@ sudo meson install -C build
 log "Refreshing linker cache"
 sudo ldconfig
 
-log "Reinstall complete"
-echo "You should reboot now."
-echo "Command: sudo reboot"
+log "Repair install completed"
+echo
+echo "[SUCCESS] libcamera and rpicam-apps were reinstalled."
+echo "[SUCCESS] Please reboot now with:"
+echo
+echo "sudo reboot"
